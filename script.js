9559d7b8cc659a0ebb11aa0e6d453be4cd2820f7
nipplelion <piolotorrico@yahoo.com>
2024-06-07 17:14:56 -0700

    feat: add ship and gameboard classes

---
+++ script.js
@@ -0,0 +1,91 @@
+class Ship {
+  constructor(length, numHits = 0, sunk = false) {
+    this.length = length;
+    this.numHits = numHits;
+    this.sunk = sunk;
+  }
+
+  hit() {
+    this.numHits += 1;
+  }
+
+  isSunk() {
+    if (this.numHits >= this.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+class Gameboard {
+  constructor() {
+    // 10 x 10 board
+    this.board = Array.from({ length: 10 }, () => Array(10).fill(null));
+    this.alreadyHit = new Set();
+    this.shipsLeft = 0;
+  }
+
+  placeShip(x, y, ship) {
+    if (
+      x < 0 ||
+      x >= this.board.length ||
+      y < 0 ||
+      y >= this.board.length ||
+      this.board[x][y] instanceof Ship
+    ) {
+      return false;
+    }
+
+    this.board[x][y] = ship;
+    this.shipsLeft += 1;
+    return true;
+  }
+
+  getCoordString(x, y) {
+    return `${x},${y}`;
+  }
+
+  receiveAttack(x, y) {
+    if (
+      x < 0 ||
+      x >= this.board.length ||
+      y < 0 ||
+      y >= this.board.length ||
+      this.alreadyHit.has(this.getCoordString(x, y))
+    ) {
+      return false;
+    }
+
+    this.alreadyHit.add(this.getCoordString(x, y));
+
+    if (this.board[x][y] instanceof Ship) {
+      this.board[x][y].hit();
+      this.updateShipSink(this.board[x][y]);
+      return true;
+    }
+
+    return false;
+  }
+
+  updateShipSink(ship) {
+    const isSunk = ship.isSunk();
+    if (isSunk) {
+      ship.sunk = isSunk;
+      this.shipsLeft -= 1;
+    }
+  }
+
+  allShipsSunk() {
+    if (this.shipsLeft <= 0) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class Player {
+  constructor() {}
+}
+
+module.exports = { Ship, Gameboard };
---
+++ script.test.js
@@ -0,0 +1,73 @@
+const { Ship, Gameboard } = require("./script.js");
+
+describe("Ship", () => {
+  test("should create a ship w/ default values", () => {
+    const testShip = new Ship(3);
+    expect(testShip.length).toBe(3);
+    expect(testShip.numHits).toBe(0);
+    expect(testShip.sunk).toBe(false);
+  });
+
+  test("ship hits should increase when called", () => {
+    const testShip = new Ship(3, 0, false);
+    expect(testShip.numHits).toBe(0);
+    testShip.hit();
+    expect(testShip.numHits).toBe(1);
+  });
+
+  test("ship function should show sunk after being hit", () => {
+    const testShip = new Ship(3, 2, false);
+    expect(testShip.isSunk()).toBe(false);
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(true);
+  });
+});
+
+describe("Gameboard", () => {
+  test("board should create a 10x10 matrix filled with null", () => {
+    const testBoard = new Gameboard();
+    expect(testBoard.board.length).toBe(10);
+    for (let i = 0; i < testBoard.board.length; i++) {
+      expect(testBoard.board[i].length).toBe(10);
+    }
+  });
+
+  test("board should accept only valid ship placements", () => {
+    const testBoard = new Gameboard();
+    const ship = new Ship(1, 0, false);
+    // coordinates out of range
+    expect(testBoard.placeShip(-1, 0, ship)).toBe(false);
+    expect(testBoard.placeShip(0, -1, ship)).toBe(false);
+    expect(testBoard.placeShip(0, 11, ship)).toBe(false);
+    expect(testBoard.placeShip(11, 0, ship)).toBe(false);
+
+    // placing on the same coordinate twice
+    expect(testBoard.placeShip(0, 0, ship)).toBe(true);
+    expect(testBoard.placeShip(0, 0, ship)).toBe(false);
+  });
+
+  test("board can receive attacks on ships and empty cells", () => {
+    const testBoard = new Gameboard();
+    const ship = new Ship(1, 0, false);
+    testBoard.placeShip(0, 0, ship);
+
+    expect(testBoard.receiveAttack(1, 1)).toBe(false);
+    expect(testBoard.alreadyHit.has("1,1")).toBe(true);
+
+    expect(testBoard.receiveAttack(0, 0)).toBe(true);
+    expect(testBoard.receiveAttack(0, 0)).toBe(false);
+    expect(testBoard.alreadyHit.has("0,0")).toBe(true);
+
+    expect(ship.numHits).toBe(1);
+    expect(ship.sunk).toBe(true);
+  });
+
+  test("board can determine if all ships are sunk", () => {
+    const testBoard = new Gameboard();
+    const ship = new Ship(1, 0, false);
+    testBoard.placeShip(0, 0, ship);
+    expect(testBoard.allShipsSunk()).toBe(false);
+    testBoard.receiveAttack(0, 0);
+    expect(testBoard.allShipsSunk()).toBe(true);
+  });
+});
